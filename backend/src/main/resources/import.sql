842ad257aa521212f7acdb96f374f906672fe56e
rebeka-batista <rebekabatista05@outlook.com>
2022-06-15 15:05:43 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://blog.quick.com.br/wp-content/uploads/2022/04/image-from-rawpixel-id-6341207-jpeg-scaled.jpg', 'http://www.microcampcentro.com.br/assets/images/courses/thumb_web.jpg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-20T03:00:00Z', 1);
