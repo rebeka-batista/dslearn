ebbf858b72dfd660c0f1a569d2518bcc83907ada
rebeka-batista <rebekabatista05@outlook.com>
2022-06-16 12:45:15 -0300

    Deliver

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_notification")
 public class Notification implements Serializable {
-    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
