e8cddfec46d39b42a5c76ae94644717859fbc51a
rebeka-batista <rebekabatista05@outlook.com>
2022-06-17 15:42:43 -0300

    Custom content for current user

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/NotificationDto.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dslearnbds.dto;
+
+import com.devsuperior.dslearnbds.entities.Notification;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class NotificationDto implements Serializable {
+
+    private Long id;
+    private String text;
+    private Instant moment;
+    private boolean read;
+    private String route;
+    private Long userId;
+
+    public NotificationDto() {
+
+    }
+
+    public NotificationDto(Long id, String text, Instant moment,
+                           boolean read, String route, Long userId) {
+        this.id = id;
+        this.text = text;
+        this.moment = moment;
+        this.read = read;
+        this.route = route;
+        this.userId = userId;
+    }
+
+    public NotificationDto(Notification entity) {
+        this.id = entity.getId();
+        this.text = entity.getText();
+        this.moment = entity.getMoment();
+        this.read = entity.isRead();
+        this.route = entity.getRoute();
+        this.userId = entity.getUser().getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+}
